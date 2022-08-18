579d4b318bd6e8348c60232d60d9a3bc235ec47b
JimDong <jimdong@damaiapp.com.tw>
2022-08-18 16:23:09 +0800

    add count on header

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Header() {
+  const [count, setCount] = useState(0)
   return (
     <div className="sticky top-0 flex gap-5 bg-blue-300 p-5 text-white">
       <Link href="/">
@@ -15,6 +16,15 @@ export default function Header() {
       <Link href="/static">
         <a>Static</a>
       </Link>
+
+      <div
+        className="ml-auto cursor-pointer rounded bg-green px-5 text-center"
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >
+        {count}
+      </div>
     </div>
   )
 }
