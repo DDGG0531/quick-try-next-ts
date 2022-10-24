7915fef6d8b22a5e4aef35a7f56fb87dc1a3376b
JimDong <jimdong@damaiapp.com.tw>
2022-10-24 18:29:00 +0800

    try

--- next.config.js
+++ next.config.js
@@ -9,7 +9,27 @@ const nextConfig = {
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true
+  },
+  webpack: config => {
+    config.optimization.minimize = false
+    return config
   }
 }
 
-module.exports = nextConfig
+const fs = require('fs')
+const getDirectories = source =>
+  fs
+    .readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => `@chakra-ui/${dirent.name}`)
+
+const arr = getDirectories('node_modules/@chakra-ui')
+const withTM = require('next-transpile-modules')([...arr]) // pass the modules you would like to see transpiled
+
+// const packageJSON = require('./package.json')
+// const transpiledPackages = Object.keys(packageJSON.dependencies).filter(it =>
+//   it.includes('@chakra-ui/')
+// )
+// const withTM = require('next-transpile-modules')(transpiledPackages)
+
+module.exports = withTM(nextConfig)
